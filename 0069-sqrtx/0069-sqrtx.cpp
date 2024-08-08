50c993714dd4a4afd49a9412bf9ff49f68f7a838
BasitSol <sigmabphile1052@gmail.com>
2024-08-08 14:25:12 +0500

    Time: 0 ms (100.00%), Space: 8.3 MB (8.23%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int ans = 0;
+        long long  lo = 0, hi = x;
+        while(lo <= hi)
+        {
+            long long  mid = lo + (hi - lo) / 2;
+            if(mid*mid == x) return mid ;
+            else if(mid * mid > x) hi = mid - 1;
+            else {
+                lo = mid + 1;
+                ans = mid ;
+            }
+        }
+        return ans ;   
+    }
+};
